a66992d7f9cb40bc39c21de84014b2dd698ee4a7
ATXprod <92235226+ATXprod@users.noreply.github.com>
2021-10-10 00:48:56 +0100

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,5 @@
+LocalPlayer.Name = ("ATXXX Beats");
+LocalPlayer.Career = ("Music Producer/Developer");
+LocalPlayer.Age = 15;
+LocalPlayer.MusicCredits = ("Produced For: DTG, Chappo, Screama, Niko B, Isong, Gully, Kwengface + more");
+LocalPlayer.Listens = (> 2,200,000);
